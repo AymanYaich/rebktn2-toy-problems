a1bb66654a1e13803bd6af3aa86798f24520bb43
Ayman <aymenyaiche@yahoo.fr>
2020-03-13 09:42:45 +0100

    Second toy problem

--- isSubSetOf/isSubSetOf.js
+++ isSubSetOf/isSubSetOf.js
@@ -20,5 +20,35 @@
  * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
 */
 Array.prototype.isSubsetOf = function (arr) {
-    // your code here
-};
+    // your code here 
+
+    var arrayNonRepeat=Array[0]; // this array will recieve the non repeated elements of the input array
+     var k=0;
+     var h=0;
+    for ( var i = 0 ; i < Array.length ; i ++ ) {
+
+        for ( var j = i+1 ; j< Array.length ; j++){
+            if ( !arrayNonRepeat.includes[Array[j]]){
+                arrayNonRepeat[k]= Array[j]
+                k=k+1
+            }
+            
+
+        }
+    }
+    for ( var d=0 ; d<arrayNonRepeat.length; d++){
+        for ( var l=0 ; l< arr.length ; i++){
+            if (arrayNonRepeat[d]=arr[l]){
+                h=h+1
+            }
+
+        }
+        
+        }
+        if ( h>=k){
+            return true
+        }
+        return false
+        
+    }
+    
